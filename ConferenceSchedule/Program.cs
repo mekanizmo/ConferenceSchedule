5a005a2c3d81e0e6ee775d5e0cf86dc78f85b380
Pedro de Sá <mekanizmo@gmail.com>
2021-03-06 14:51:10 +0100

    updated add talks to tracks

--- ConferenceSchedule/Program.cs
+++ ConferenceSchedule/Program.cs
@@ -7,19 +7,14 @@ namespace ConferenceSchedule
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.Clear();
 
-            Schedule schedule = new Schedule();
-            Track initTrack = new Track { Id = 1, Date = schedule.StartDate };
-            schedule.Tracks.Add(initTrack);
-            var targetTrack = schedule.Tracks.Last();
-
-            Talk lunch = new Talk { Title = "Lunch", Duration = 1, StartTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 12, 0, 0), EndTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 13, 0, 0) };
-            Talk network = new Talk { Title = "Network Event", StartTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 17, 0, 0) };
+            Schedule schedule = new Schedule { StartDate = DateTime.Today };
+            
+            Talk lunch = new Talk { Title = "Lunch", Duration = 60, StartTime = new DateTime(schedule.StartDate.Year, schedule.StartDate.Month, schedule.StartDate.Day, 12, 0, 0), EndTime = new DateTime(schedule.StartDate.Year, schedule.StartDate.Month, schedule.StartDate.Day, 13, 0, 0) };
+            Talk network = new Talk { Title = "Network Event", StartTime = new DateTime(schedule.StartDate.Year, schedule.StartDate.Month, schedule.StartDate.Day, 17, 0, 0) };
 
             Console.WriteLine("Conference Schedule\n----------------");
             Console.Write("[F] Import .TXT file  \n[M] Enter Mannualy\n[X] Exit\nChoose your input method (F/M/X): ");
@@ -29,35 +24,54 @@ namespace ConferenceSchedule
                 case "F":
                     // import txt file
                     var talks = ImportFromFile();
+                    Track targetTrack = new Track { Id = 1, Date = schedule.StartDate };
+                    schedule.Tracks.Add(targetTrack);
                     foreach (var talk in talks)
                     {
-                        if (targetTrack.Talks.Count > 0)
+                        if (targetTrack.Talks.Any())
                         {
                             var lastTalk = schedule.Tracks.Last().Talks.Last();
-                            Console.WriteLine(lastTalk.EndTime.ToString());
-                            if (talk.EndTime > network.StartTime)
+                            talk.StartTime = lastTalk.EndTime;
+                            talk.EndTime = talk.StartTime.AddMinutes(talk.Duration);
+
+                            if (talk.EndTime >= lunch.StartTime && talk.EndTime <= lunch.EndTime)
                             {
+                                AddTalkToTrack(lunch, targetTrack);
+
+                                talk.StartTime = lunch.EndTime;
+                                talk.EndTime = talk.StartTime; talk.EndTime.AddMinutes(talk.Duration);
+                            }
+                            else if (talk.StartTime >= lunch.EndTime && talk.EndTime >= network.StartTime)
+                            {
+                                AddTalkToTrack(network, targetTrack);
                                 schedule.Tracks.Add(new Track { Id = targetTrack.Id + 1, Date = targetTrack.Date.AddDays(1) });
                                 targetTrack = schedule.Tracks.Last();
-                                //network.StartTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 17, 0, 0);
+
+                                network.StartTime = network.StartTime.AddDays(1);
+
+                                lunch.StartTime = lunch.StartTime.AddDays(1);
+                                lunch.EndTime = lunch.StartTime.AddMinutes(lunch.Duration);
+
+                                talk.StartTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 9, 0, 0);
+                                talk.EndTime = talk.StartTime.AddMinutes(talk.Duration);
                             }
-                            //else if(lastTalk.EndTime > lunch.StartTime && lastTalk.StartTime < lunch.EndTime)
-                            //{
-                            //    AddTalkToTrack(lunch, targetTrack);
-                            //}
+                            AddTalkToTrack(talk, targetTrack);
 
                         }
-
-
-                        AddTalkToTrack(talk, targetTrack);
+                        else
+                        {
+                            talk.StartTime = new DateTime(targetTrack.Date.Year, targetTrack.Date.Month, targetTrack.Date.Day, 9, 0, 0);
+                            talk.EndTime = talk.StartTime.AddMinutes(talk.Duration);
+                            AddTalkToTrack(talk, targetTrack);
+                        }
                     }
                     break;
                 case "M":
                     // read input
                     Console.WriteLine("Title and duration (in minutes):");
-                    var talkInput = ConvertToTalk(Console.ReadLine());
-                    targetTrack = schedule.Tracks.Last();
-                    AddTalkToTrack(talkInput, targetTrack);
+                    //var talkInput = ConvertToTalk(Console.ReadLine());
+                    //targetTrack = schedule.Tracks.Last();
+                    //AddTalkToTrack(talkInput, targetTrack);
                     break;
                 case "X":
                     // exit program
@@ -73,13 +87,14 @@ namespace ConferenceSchedule
 
         }
 
-        static List<Talk> ImportFromFile() //TEST: \Projects\ConferenceSchedule\TestInput.txt
-        {
+        /*TEST: 
+        \Projects\ConferenceSchedule\TestInput.txt
+        */
 
+        static List<Talk> ImportFromFile()
+        {
             Console.WriteLine("File location path:");
             string path = Console.ReadLine();
-
-            List<Talk> talks = new List<Talk>();
 
             if (!File.Exists(path))
             {
@@ -88,79 +103,52 @@ namespace ConferenceSchedule
             }
             else
             {
-                string[] tracks = File.ReadAllLines(path);
-                foreach (string line in tracks)
-                {
-                    talks.Add(ConvertToTalk(line));
-                }
-                return talks;
+                return ConvertToTalks(File.ReadAllLines(path));
             }
-
         }
 
-        static Talk ConvertToTalk(string inputTalk)
+        static List<Talk> ConvertToTalks(string[] inputTalks)
         {
-            bool valid = false;
-            Talk newTalk = new Talk
+            List<Talk> listTalk = new List<Talk>();
+
+
+            foreach (var talk in inputTalks)
             {
-                Title = inputTalk
-            };
-            string tempDuration = inputTalk.Split(' ').Last().ToLower();
-            if (tempDuration == "lightning")
-            {
-                newTalk.Duration = 5;
-                valid = true;
-            }
-            else
-            {
-                try
+
+                bool success = false;
+                Talk newTalk = new Talk
                 {
-                    newTalk.Duration = Convert.ToInt32(tempDuration.Split("min").First());
-                    valid = true;
-                }
-                catch (Exception e)
+                    Title = talk
+                };
+                string tempDuration = talk.Split(' ').Last().ToLower();
+                if (tempDuration == "lightning")
                 {
-                    Console.WriteLine("Invalid duration input!");
+                    newTalk.Duration = 5;
+                    success = true;
+                }
+                else
+                {
+                    try
+                    {
+                        newTalk.Duration = Convert.ToInt32(tempDuration.Split("min").First());
+                        success = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid duration input!");
+                    }
+                }
+                if (success)
+                {
+                    listTalk.Add(newTalk);
                 }
             }
 
-
-            if (valid) { return newTalk; }
-            else { return null; }
-
+            return listTalk;
         }
 
         static void AddTalkToTrack(Talk newTalk, Track track)
         {
-
-            if (track.Talks.Count <= 0)
-            {
-                newTalk.StartTime = new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 9, 0, 0);
-                newTalk.EndTime = newTalk.StartTime.AddMinutes(newTalk.Duration);
-            }
-            else
-            {
-                newTalk.StartTime = track.Talks.Last().StartTime.AddMinutes(track.Talks.Last().Duration);
-                newTalk.EndTime = newTalk.StartTime.AddMinutes(newTalk.Duration);
-
-                if (newTalk.EndTime > new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 12, 0, 0) && newTalk.EndTime <= new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 13, 0, 0))
-                {
-                    Talk lunch = new Talk { Title = "Lunch", Duration = 1, StartTime = new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 12, 0, 0) };
-                    lunch.EndTime = lunch.StartTime.AddMinutes(lunch.Duration);
-                    track.Talks.Add(lunch);
-
-                    newTalk.StartTime = new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 13, 0, 0);
-                    newTalk.EndTime = newTalk.StartTime.AddMinutes(newTalk.Duration);
-                }
-
-                else if (newTalk.EndTime > new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 17, 0, 0))
-                {
-                    Talk network = new Talk { Title = "Network Event", StartTime = new DateTime(track.Date.Year, track.Date.Month, track.Date.Day, 17, 0, 0) };
-                    track.Talks.Add(network);
-
-                }
-
-            }
 
             track.Talks.Add(newTalk);
         }
